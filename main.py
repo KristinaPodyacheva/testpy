e659e259372b1422ae3b68c592e02fc1e0e98ee1
student <redfielcom@mail.ru>
2022-12-03 12:33:18 +0300

    1

---
+++ main.py
@@ -0,0 +1,10 @@
+import re
+
+s = input("Введите строку")
+count = 0
+vowels = set("aeiou")
+for letters in s:
+    if letters in vowels:
+        count += 1
+    print("Количество гласных равно:")
+    print(count)
